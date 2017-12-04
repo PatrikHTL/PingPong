3e7678106f6b0f7375a05d098c76648ec49dda76
Hasi <ahaslehn@htl-steyr.ac.at>
2017-12-04 11:30:13 +0100

    Ball, Schläger prototyp

--- src/PingPong/Schlaeger.java
+++ src/PingPong/Schlaeger.java
@@ -1,4 +1,26 @@
 package PingPong;
 
+import gui.Zeichnung;
+
 public class Schlaeger {
+    private final double Xcord;
+    private double Ycord;
+
+    public Schlaeger(double Xcord) {    //Constructer weil 2 Schläger existieren
+        this.Xcord=Xcord;
+        Zeichnung.setSchlaegerPosition();
+    }
+
+    public void setYcord(double ycord){
+        Ycord=ycord;
+        Zeichnung.setSchlaegerPosition();
+    }
+
+    public double getXcord() {
+        return Xcord;
+    }
+
+    public double getYcord() {
+        return Ycord;
+    }
 }
