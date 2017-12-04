13c2e1871510f41e0d1b8f7686e00d28cca57c43
PatrikHTL <preitner@htl-steyr.ac.at>
2017-12-04 11:35:18 +0100

    diverse Änderungen mündlich besprochen

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -15,16 +15,10 @@ public class Application {
 
         // Center the window
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Dimension frameSize = frame.getSize();
-        if (frameSize.height > screenSize.height) {
-            frameSize.height = screenSize.height;
-        }
-        if (frameSize.width > screenSize.width) {
-            frameSize.width = screenSize.width;
-        }
-        frame.setLocation((screenSize.width - frameSize.width) / 2,
-                (screenSize.height - frameSize.height) / 2);
+        frame.setLocation((screenSize.width - 1000) / 2,
+                (screenSize.height - 700) / 2);
         frame.setVisible(true);
+        frame.setResizable(false);
     }
 
     /**
